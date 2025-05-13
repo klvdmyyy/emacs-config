d15352d51c1eacbc0bc4c0c220242ed6f67d0fd9
Dmitry <klementievd08@yandex.ru>
2025-05-13 22:02:10 +0500

    Initial commit

--- system/programs/hyprland/rules.nix
+++
@@ -1,6 +0,0 @@
-{
-  lib,
-  ...
-}: {
-  # TODO: https://github.com/fufexan/dotfiles/blob/main/system/programs/hyprland/rules.nix
-}
--- system/programs/hyprland/smartgaps.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-in {
-  # TODO: https://github.com/fufexan/dotfiles/blob/main/system/programs/hyprland/smartgaps.nix
-}
