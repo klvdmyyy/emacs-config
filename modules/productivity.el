4224060fd42bd12029a71f63ff6418f8e39296d5
Dmitry <klementievd08@yandex.ru>
2025-05-23 15:53:05 +0500

    Some changes

--- modules/productivity.el
+++ modules/productivity.el
@@ -4,11 +4,20 @@
 ;;
 ;;; Code:
 
+(dolist (package '())
+  (straight-use-package package))
+
 ;; [TODO] Move `org-mode-indent' to appearance maybe !?
 ;; [TODO] Move `visual-line-mode' to appearance maybe !?
 (dolist (mode '(org-indent-mode
                 visual-line-mode))
   (add-hook 'org-mode-hook mode))
+
+(setq org-agenda-files
+      '("~/Projects/Org/Tasks.org"
+        "~/Projects/Org/Birthdays.org"
+        ;;"~/Projects/Org/Habits.org"
+        ))
 
 (provide 'productivity)
 
