e972c67414bbdb1b24f8f467921bb64fdbf91c3d
Dmitry <klementievd08@yandex.ru>
2025-05-23 13:32:21 +0500

    Some eglot tweaks

--- modules/tweaks.el
+++ modules/tweaks.el
@@ -13,6 +13,8 @@
 (with-eval-after-load 'gcmh
   (gcmh-mode 1))
 
+(fset #'jsonrpc--log-event #'ignore)    ; Maybe good option for making Eglot faster
+
 (provide 'tweaks)
 
 ;;; tweaks.el ends here
